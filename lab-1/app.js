08ec6a2045d7066e98629edef8b00a939a15b0dd
Eyasu Gebrehiwot <jgebrehiwot@gmail.com>
2021-06-23 11:12:13 -0500

    some corrections on lab-1"

--- lab-1/app.js
+++ lab-1/app.js
@@ -1,14 +1,22 @@
 //Write the necessary Node script to make this code work for all arrays: [1, 2, 3, 4, 5, 6, 7, 8].even(); // [2,4,6,8] [1,2,3,4,5,6,7,8].odd(); // [1,3,5,7] Test your code in Node.JS CLI
-Array.prototype.even = function (arr) {
-    return arr.filter((element) => {
-        element % 2 === 0;
-    })
-}
-[1, 2, 4, 5, 6, 7, 8].even();
+let arr = [1, 2, 3, 4, 5, 6];
 
+Array.prototype.even = function () {
+    let arr2= [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            arr2.push(arr[i])
+        }
+    }
+    return arr2;
+}
+let temp = arr.even();
+console.log(temp);
 
 Array.prototype.odd = function () {
-   return Array.filter((element) => {
-        element % 2 !== 0;
-    })
+   return this.filter((element) => 
+        element % 2 !== 0 )
 }
+let arr3 = [1, 3, 6, 7, 8, 9];
+temp= arr3.odd();
+console.log(temp);
